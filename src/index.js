e94b829e050e6efe5c4eb9c76712c0e4cdda5118
MdSourovAhmed <96908951+MdSourovAhmed@users.noreply.github.com>
2026-02-19 00:09:05 +0600

    Featuring websocket properties

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import http from "http";
 import { matchRouter } from "./routes/matches.js";
+import { attachWebSocketServer } from "./ws/server.js";
+
 
 dotenv.config();
 
-const server = express();
+const app = express();
+const server = http.createServer(app);
 
-server.use(cors());
-server.use(express.json());
+app.use(cors());
+app.use(express.json());
 
-server.get("/health", async (req, res) => {
+app.get("/health", async (req, res) => {
   res.status(200).json({ message: "Server is in good health" });
 });
 // server.post("/matches", async (req, res) => {
@@ -19,9 +23,19 @@ server.get("/health", async (req, res) => {
 //     .json({ message: "Server is in good health"});
 // });
 
-server.use("/matches", matchRouter);
+app.use("/matches", matchRouter);
+
+const { broadcastMatchCreated } = attachWebSocketServer(server);
+app.locals.broadcastMatchCreated = broadcastMatchCreated;
 
 const PORT = process.env.PORT || 7000;
-server.listen(PORT, () => {
-  console.log(`Server is running at: http://locathost:${PORT}`);
+const HOST = process.env.HOST || "0.0.0.0";
+server.listen(PORT,HOST, () => {
+  const baseUrl =
+    HOST === "0.0.0.0" ? `http://localhost:${PORT}` : `http://${HOST}:${PORT}`;
+
+  console.log(`Server is running at: ${baseUrl}`);
+  console.log(
+    `WebSocket Server is running at: ${baseUrl.replace("http", "ws")}/ws`,
+  );
 });
